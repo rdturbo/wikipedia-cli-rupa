ae542afb31abbd3a2aac2e797174e36170e9c57c
rdturbo <rupayan98@gmail.com>
2022-08-29 04:46:34 -0400

    Add wikipedia

--- src/wikipedia_cli_rupa/console.py
+++ src/wikipedia_cli_rupa/console.py
@@ -1,27 +1,18 @@
 # src/wikipedia_cli_rupa/console.py
 import textwrap
 import click
-import requests
 
-from . import __version__
-
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+from . import __version__, wikipedia
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The Wikipedia Command Line Interface project."""
-    try:
-        with requests.get(API_URL) as response:
-            response.raise_for_status()
-            data = response.json()
-    except Exception as err:
-        print("Please check the url again")
-    else:
-        title = data["title"]
-        extract = data["extract"]
+    data = wikipedia.random_page()
+    
+    title = data["title"]
+    extract = data["extract"]
 
-        click.secho(title, fg="green")
-        click.echo(textwrap.fill(extract))
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
