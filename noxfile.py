4f757126ed6f97f5cc835290a5215534485ab3cb
rdturbo <rupayan98@gmail.com>
2022-08-29 06:48:53 -0400

    Add pre-commit-hook

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import tempfile
 import nox
 
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -32,9 +32,30 @@ def black(session):
 def lint(session):
     args = session.posargs or locations
     install_with_constraints(
-        session, "flake8", "flake8-black", "flake8-bugbear", "flake8-import-order"
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
     )
     session.run("flake8", *args)
+
+
+@nox.session(python="3.10")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.10"])
